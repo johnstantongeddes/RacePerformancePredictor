d6703e4fd718bcae8103286fb3b703c6b5332d2e
John Stanton-Geddes <john.stantongeddes.research@gmail.com>
2015-06-08 22:34:45 -0400

    updated analysis with test set
    added git lfs for data file storage

--- R/toHMS.R
+++ R/toHMS.R
@@ -4,7 +4,11 @@ toHMS <- function(x){
   
   H <- formatC(x %/% 3600, width=2, flag="0")
   M <- formatC(x %% 3600 %/% 60, width=2, flag="0")
-  S <- formatC(x %% 60, width=2, flag="0", digits=2)
+  seconds <- x %% 60
+  # formatting for seconds
+  if(seconds < 10) { 
+    S <- formatC(x %% 60, width=2, flag="0", digits=1) } else 
+    S <- formatC(x %% 60, width=2, flag="0", digits=2)
   
   paste(H, M, S, sep = ":")
 } 
